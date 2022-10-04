42c88046260ef2f82b3266ebb3ffd2d4079335a2
nafnaufal <naufalanbial@gmail.com>
2022-10-04 08:59:19 +0700

    favorite

--- lib/favorite.dart
+++ lib/favorite.dart
@@ -1,24 +1,17 @@
 import 'package:flutter/material.dart';
 
-class Favorite extends InheritedWidget{
+class Favorite extends InheritedWidget {
   final MyInheritedData isFavorite;
   final Widget child;
 
-  const Favorite({
-      Key? key, 
-      required this.isFavorite, 
-      required this.child
-    })
-      : super(
-          key: key, 
-          child: child);
-
+  const Favorite({Key? key, required this.isFavorite, required this.child})
+      : super(key: key, child: child);
 
   static Favorite? of(BuildContext context) =>
-    context.dependOnInheritedWidgetOfExactType<Favorite>();
+      context.dependOnInheritedWidgetOfExactType<Favorite>();
 
   @override
-  bool updateShouldNotify(Favorite oldWidget){
+  bool updateShouldNotify(Favorite oldWidget) {
     return isFavorite != oldWidget.isFavorite;
   }
 }
